f3ed72cc5019f4eeba78d06f832d1c6ddeb63913
StephonT <Stephonfresh@aol.com>
2022-05-22 13:59:45 -0400

    created password length,  prompt, and password length conditional

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Assignment code here
 
+//Generate Button Function
+function generatePassword() {
+  //Set Password Length and prompt
+  var setPasswordLength = prompt(
+    "Please choose a number between 8 and 128 for your desired password length."
+  );
+
+  //Set Password Length As Number
+  var lengthAsNumber = parseInt(setPasswordLength);
+
+  console.log(lengthAsNumber);
+
+  //Create conditional statement for password length
+  if (lengthAsNumber > 8 && lengthAsNumber < 128) {
+    confirm("You have selected a character length of " + lengthAsNumber + ".");
+  } else if (lengthAsNumber <= 8 || lengthAsNumber >= 128) {
+    alert(
+      "You did not choose a number between 8 and 128. Press 'Generate Password' button to restart. Thank you!"
+    );
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
