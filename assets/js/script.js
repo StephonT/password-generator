dc11a3a6d81341ecf6dae5402172ae65f1781f6f
StephonT <Stephonfresh@aol.com>
2022-05-23 15:01:36 -0400

    created password length variable and prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,114 @@
+lowerCaseChars =
+  ("a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z");
+
+upperCaseChars =
+  ("A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z");
+
+numberChars = (1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+specialChars =
+  ("\u0020",
+  "\u0021",
+  "\u0022",
+  "\u0023",
+  "\u0024",
+  "\u0025",
+  "\u0026",
+  "\u0027",
+  "\u0028",
+  "\u0029",
+  "\u002A",
+  "\u002B",
+  "\u002C",
+  "\u002D",
+  "\u002E",
+  "\u002F",
+  "\u003A",
+  "\u003B",
+  "\u003C",
+  "\u003D",
+  "\u003E",
+  "\u003F",
+  "\u0040",
+  "\u005B",
+  "\u005C",
+  "\u005D",
+  "\u005E",
+  "\u005F",
+  "\u0060",
+  "\u007B",
+  "\u007C",
+  "\u007D",
+  "\u007E");
+
 // Assignment code here
+function generatePassword() {
+  // Defining How Many Characters User Wants In Generated Password
+  passwordLength = prompt(
+    "Please choose a number between 8 and 128 for your desired password length."
+  );
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    confirm("You have chosen a character length of " + passwordLength);
+  } else if (
+    passwordLength < 8 ||
+    passwordLength > 128 ||
+    passwordLength === NaN
+  ) {
+    alert("You did not enter a valid number. Please try again.");
+    generatePassword();
+  }
+  //console.log(passwordLength); // To see Password Length in Log
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
